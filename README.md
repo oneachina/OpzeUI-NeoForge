f7dd3d95f14e64624fe6ddcc475e817e546418a3
oneachina <ruiruixi@hotmail.com>
2025-02-10 15:19:12 +0800

    更改名字

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# SomeItem Mod
+# OpzeUI Mod
+
+<br />
+<p align="center">
+  <h3 align="center">一个为Minecraft整合的uimod</h3>
+  <p align="center">
+    <br />
+    <a href="https://github.com/oneachina/OpzeUI-NeoForge">查看本仓库</a>
+    ·
+    <a href="https://github.com/oneachina/OpzeUI-NeoForge/issues">报告 Bug 或提出特性</a>
+    ·
+    <a href="https://github.com/oneachina/OpzeUI-NeoForge/pulls">提出拉取请求</a>
+</p>
 
 ## Warning
 
-模组的食物是Food Enhancements mod的，非官方移植版，不保证与原版一致
-
-## 计划
-
-* [ ]  移植完[食物扩增 (Food Enhancements)](https://www.mcmod.cn/class/7298.html)中的食物🎉️
-* [ ]  添加一些其他模组的食物（仅移植断更的，想着把那些断更的也能在高版本存活)😄
+本版本为Neoforge！
 
 ## License
 
---
+++ src/main/java/cn/opze/ui/OpzeUI.java
@@ -0,0 +1,44 @@
+package cn.opze.ui;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(OpzeUI.MODID)
+public class OpzeUI
+{
+    public static final String MODID = "opzeuI";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public OpzeUI(IEventBus modEventBus, ModContainer modContainer)
+    {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("OPZEUI SETUP");
+        }
+    }
+}
